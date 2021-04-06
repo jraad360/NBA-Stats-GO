f628c49b96bd61446ff535789c111403c21207ac
dwl23@duke.edu <dwl23@duke.edu>
2021-04-06 04:42:51 -0400

    Add Home View Controller

---
+++ NBAStatsGo/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  NBAStatsGo
+//
+//  Created by David Liu on 4/6/21.
+//
+
+import UIKit
+
+
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
