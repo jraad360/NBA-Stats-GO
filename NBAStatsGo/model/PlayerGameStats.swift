a2c0630046d678af35a58f2e8eadc09b92d2ff43
jraad360 <jar121@duke.edu>
2021-04-07 14:50:23 -0400

    created json initializers for PlayerGameStats and PlayerSeasonAverageStats

--- NBAStatsGo/model/PlayerGameStats.swift
+++ NBAStatsGo/model/PlayerGameStats.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct PlayerGameStats {
     let player: Player
@@ -31,4 +32,79 @@ struct PlayerGameStats {
     let ftpct: Double
     let turnover: Int
     let pf: Int
+    
+    init(
+        player: Player,
+        team: Team,
+        game: Game,
+        min: Int,
+        pts: Int,
+        ast: Int,
+        reb: Int,
+        dreb: Int,
+        oreb: Int,
+        stl: Int,
+        blk: Int,
+        fga: Int,
+        fgm: Int,
+        fgpct: Double,
+        fg3a: Int,
+        fg3m: Int,
+        fg3pct: Double,
+        fta: Int,
+        ftm: Int,
+        ftpct: Double,
+        turnover: Int,
+        pf: Int) {
+        
+        self.player = player
+        self.team = team
+        self.game = game
+
+        self.min = min
+        self.pts = pts
+        self.ast = ast
+        self.reb = reb
+        self.dreb = dreb
+        self.oreb = oreb
+        self.stl = stl
+        self.blk = blk
+        self.fga = fga
+        self.fgm = fgm
+        self.fgpct = fgpct
+        self.fg3a = fg3a
+        self.fg3m = fg3m
+        self.fg3pct = fg3pct
+        self.fta = fta
+        self.ftm = ftm
+        self.ftpct = ftpct
+        self.turnover = turnover
+        self.pf = pf
+    }
+    
+    init(json: JSON) throws {
+        self.init(
+            player: try Player(json: json["player"]),
+            team: try Team(json: json["team"]),
+            game: try Game(json: json["game"]),
+            min: json["min"].int!,
+            pts: json["pts"].int!,
+            ast: json["ast"].int!,
+            reb: json["reb"].int!,
+            dreb: json["dreb"].int!,
+            oreb: json["oreb"].int!,
+            stl: json["stl"].int!,
+            blk: json["blk"].int!,
+            fga: json["fga"].int!,
+            fgm: json["fgm"].int!,
+            fgpct: json["fg_pct"].double!,
+            fg3a: json["fg3a"].int!,
+            fg3m: json["fg3m"].int!,
+            fg3pct: json["fg3_pct"].double!,
+            fta: json["fta"].int!,
+            ftm: json["ftm"].int!,
+            ftpct: json["ft_pct"].double!,
+            turnover: json["turnover"].int!,
+            pf: json["pf"].int!)
+    }
 }
--- NBAStatsGo/model/PlayerSeasonAverageStats.swift
+++ NBAStatsGo/model/PlayerSeasonAverageStats.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct PlayerSeasonAverageStats {
     let player: Player
@@ -32,4 +33,86 @@ struct PlayerSeasonAverageStats {
     let ftpct: Double
     let turnover: Double
     let pf: Double
+    
+    init(
+        player: Player,
+        team: Team,
+        season: String,
+        gp: Int,
+        min: Double,
+        pts: Double,
+        ast: Double,
+        reb: Double,
+        dreb: Double,
+        oreb: Double,
+        stl: Double,
+        blk: Double,
+        fga: Double,
+        fgm: Double,
+        fgpct: Double,
+        fg3a: Double,
+        fg3m: Double,
+        fg3pct: Double,
+        fta: Double,
+        ftm: Double,
+        ftpct: Double,
+        turnover: Double,
+        pf: Double) {
+        
+        self.player = player
+        self.team = team
+        self.season = season
+        self.gp = gp
+
+        self.min = min
+        self.pts = pts
+        self.ast = ast
+        self.reb = reb
+        self.dreb = dreb
+        self.oreb = oreb
+        self.stl = stl
+        self.blk = blk
+        self.fga = fga
+        self.fgm = fgm
+        self.fgpct = fgpct
+        self.fg3a = fg3a
+        self.fg3m = fg3m
+        self.fg3pct = fg3pct
+        self.fta = fta
+        self.ftm = ftm
+        self.ftpct = ftpct
+        self.turnover = turnover
+        self.pf = pf
+    }
+    
+    init(json: JSON) throws {
+        var season = json["season"].string!
+        let nextYear = Int(season)! + 1
+        season += "-\(nextYear)"
+        
+        self.init(
+            player: try Player(json: json["player"]),
+            team: try Team(json: json["team"]),
+            season: season,
+            gp: json["games_played"].int!,
+            min: json["min"].double!,
+            pts: json["pts"].double!,
+            ast: json["ast"].double!,
+            reb: json["reb"].double!,
+            dreb: json["dreb"].double!,
+            oreb: json["oreb"].double!,
+            stl: json["stl"].double!,
+            blk: json["blk"].double!,
+            fga: json["fga"].double!,
+            fgm: json["fgm"].double!,
+            fgpct: json["fg_pct"].double!,
+            fg3a: json["fg3a"].double!,
+            fg3m: json["fg3m"].double!,
+            fg3pct: json["fg3_pct"].double!,
+            fta: json["fta"].double!,
+            ftm: json["ftm"].double!,
+            ftpct: json["ft_pct"].double!,
+            turnover: json["turnover"].double!,
+            pf: json["pf"].double!)
+    }
 }
