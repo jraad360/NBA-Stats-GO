586d18de3893a43c07ec07b254b6c7a657b9bfed
jraad360 <jar121@duke.edu>
2021-04-13 03:36:39 -0400

    implemented career averages

--- NBAStatsGo/model/PlayerSeasonAverageStats.swift
+++ NBAStatsGo/model/PlayerSeasonAverageStats.swift
@@ -116,6 +116,74 @@ struct PlayerSeasonAverageStats {
             pf: json["pf"].double!)
     }
     
+    init(seasons: [PlayerSeasonAverageStats]) {
+        
+        self.playerId = seasons[0].playerId
+        self.season = "Career"
+
+        var totalGp = 0
+        var totalMin = 0.0
+        var totalPts = 0.0
+        var totalAst = 0.0
+        var totalReb = 0.0
+        var totalDreb = 0.0
+        var totalOreb = 0.0
+        var totalStl = 0.0
+        var totalBlk = 0.0
+        var totalFga = 0.0
+        var totalFgm = 0.0
+        var totalFg3a = 0.0
+        var totalFg3m = 0.0
+        var totalFta = 0.0
+        var totalFtm = 0.0
+        var totalTurnover = 0.0
+        var totalPf = 0.0
+        
+        for season in seasons {
+            totalGp += season.gp
+            totalMin += season.min * Double(season.gp)
+            totalPts += season.pts * Double(season.gp)
+            totalAst += season.ast * Double(season.gp)
+            totalReb += season.reb * Double(season.gp)
+            totalDreb += season.dreb * Double(season.gp)
+            totalOreb += season.oreb * Double(season.gp)
+            totalStl += season.stl * Double(season.gp)
+            totalBlk += season.blk * Double(season.gp)
+            totalFga += season.fga * Double(season.gp)
+            totalFgm += season.fgm * Double(season.gp)
+            totalFg3a += season.fg3a * Double(season.gp)
+            totalFg3m += season.fg3m * Double(season.gp)
+            totalFta += season.fta * Double(season.gp)
+            totalFtm += season.ftm * Double(season.gp)
+            totalTurnover += season.turnover * Double(season.gp)
+            totalPf += season.pf * Double(season.gp)
+        }
+        
+        self.gp = totalGp
+
+        self.min = totalMin / Double(totalGp)
+        self.pts = totalPts / Double(totalGp)
+        self.ast = totalAst / Double(totalGp)
+        self.reb = totalReb / Double(totalGp)
+        self.dreb = totalDreb / Double(totalGp)
+        self.oreb = totalOreb / Double(totalGp)
+        self.stl = totalStl / Double(totalGp)
+        self.blk = totalBlk / Double(totalGp)
+        self.fga = totalFga / Double(totalGp)
+        self.fgm = totalFgm / Double(totalGp)
+        self.fg3a = totalFg3a / Double(totalGp)
+        self.fg3m = totalFg3m / Double(totalGp)
+        self.fta = totalFta / Double(totalGp)
+        self.ftm = totalFtm / Double(totalGp)
+        self.turnover = totalTurnover / Double(totalGp)
+        self.pf = totalPf / Double(totalGp)
+        
+        self.fgpct = totalFgm / totalFga
+        self.fg3pct = totalFg3m / totalFg3a
+        self.ftpct = totalFtm / totalFta
+        
+    }
+    
     // TODO: move somewhere else
     static func convertMinutesToDouble(_ minuteString: String) -> Double {
         let components = minuteString.components(separatedBy: ":")
@@ -125,4 +193,5 @@ struct PlayerSeasonAverageStats {
         
         return Double(minutes) + Double(seconds)/60
     }
+    
 }
