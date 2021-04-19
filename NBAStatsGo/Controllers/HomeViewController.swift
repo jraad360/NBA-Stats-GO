4cdf72be1b7becaed3f780de77bed9aa2b1cdc81
jraad360 <jar121@duke.edu>
2021-04-19 19:11:34 -0400

    fixed animated team logos so that new view not added every time

--- NBAStatsGo/Controllers/HomeViewController.swift
+++ NBAStatsGo/Controllers/HomeViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var teamLogosView: UIView!
+    var animatedLogos: UIView? = nil
     
     // Setup the home controller
     override func viewWillAppear(_ animated: Bool) {
-        view.addSubview(createTeamLogosView())
         super.viewWillAppear(true)
+        if animatedLogos != nil {
+            animatedLogos!.removeFromSuperview()
+        }
+        animatedLogos = createTeamLogosView()
+        view.addSubview(animatedLogos!)
     }
 }
